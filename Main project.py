e580df9bbce35cbede306da15d27334705b170ab
ROJ <rojhatarr@outlook.com>
2024-07-17 20:44:37 +0100

    commit 4

--- Main project.py
+++ Main project.py
@@ -1,3 +1,16 @@
 
-def displaymenu():
-    display_menu = input ("Welcome to the To Do List\n1.Add List\n2.Remove from List\n3.Update List ")
+display_menu = input("Welcome to the To Do List\n1.Add List\n2.Remove from List\n3.Update List\n4.Delete List\n5.Show Tasks.\n6.Exit")
+class displaymenu(self):
+    def __init__(self,add,remove,update,delete,show,exit):
+        self.add = add
+        self.remove = remove
+        self.update = update
+        self.delete = delete
+        self.show = show
+        self.exit = exit
+
+
+        
+
+
+
